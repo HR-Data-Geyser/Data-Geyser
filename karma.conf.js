46c7f298f0e3fb178b9e35ee211a4bc0a245f5c3
Tom G Varik <tgvarik@me.com>
2014-11-05 10:38:03 -0800

    regenerate with new generator

--- karma.conf.js
+++ karma.conf.js
@@ -1,52 +1,80 @@
-'use strict';
-
-/**
- * Module dependencies.
- */
-var applicationConfiguration = require('./config/config');
-
 // Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
+
 module.exports = function(config) {
-	config.set({
-		// Frameworks to use
-		frameworks: ['jasmine'],
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-		// List of files / patterns to load in the browser
-		files: applicationConfiguration.assets.lib.js.concat(applicationConfiguration.assets.js, applicationConfiguration.assets.tests),
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
 
-		// Test results reporter to use
-		// Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-		//reporters: ['progress'],
-		reporters: ['progress'],
+    // list of files / patterns to load in the browser
+    files: [
+      'client/bower_components/jquery/dist/jquery.js',
+      'client/bower_components/angular/angular.js',
+      'client/bower_components/angular-mocks/angular-mocks.js',
+      'client/bower_components/angular-resource/angular-resource.js',
+      'client/bower_components/angular-cookies/angular-cookies.js',
+      'client/bower_components/angular-sanitize/angular-sanitize.js',
+      'client/bower_components/angular-route/angular-route.js',
+      'client/bower_components/lodash/dist/lodash.compat.js',
+      'client/bower_components/angular-socket-io/socket.js',
+      'client/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'client/app/app.js',
+      'client/app/app.coffee',
+      'client/app/**/*.js',
+      'client/app/**/*.coffee',
+      'client/components/**/*.js',
+      'client/components/**/*.coffee',
+      'client/app/**/*.jade',
+      'client/components/**/*.jade',
+      'client/app/**/*.html',
+      'client/components/**/*.html'
+    ],
 
-		// Web server port
-		port: 9876,
+    preprocessors: {
+      '**/*.jade': 'ng-jade2js',
+      '**/*.html': 'html2js',
+      '**/*.coffee': 'coffee',
+    },
 
-		// Enable / disable colors in the output (reporters and logs)
-		colors: true,
+    ngHtml2JsPreprocessor: {
+      stripPrefix: 'client/'
+    },
 
-		// Level of logging
-		// Possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+    ngJade2JsPreprocessor: {
+      stripPrefix: 'client/'
+    },
 
-		// Enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
+    // list of files / patterns to exclude
+    exclude: [],
 
-		// Start these browsers, currently available:
-		// - Chrome
-		// - ChromeCanary
-		// - Firefox
-		// - Opera
-		// - Safari (only Mac)
-		// - PhantomJS
-		// - IE (only Windows)
-		browsers: ['PhantomJS'],
+    // web server port
+    port: 8080,
 
-		// If browser does not capture in given timeout [ms], kill it
-		captureTimeout: 60000,
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-		// Continuous Integration mode
-		// If true, it capture browsers, run tests and exit
-		singleRun: true
-	});
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
 };
