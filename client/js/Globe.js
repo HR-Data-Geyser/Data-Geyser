4c60b7724401f275415b83c620b7917bbcbd331b
Brian Iversen <biversen21@gmail.com>
2014-11-11 16:58:33 -0800

    adds drawEdge functionality

--- client/js/Globe.js
+++ client/js/Globe.js
@@ -50,6 +50,21 @@ Globe.prototype.geoToEcef = function (lat, lon, alt) {
   return new THREE.Vector3(x,z,y);
 };
 
+Globe.prototype.getEcef = function (lat, lon, alt) {
+  var tweet = {};
+  lat *= Math.PI/180;
+  lon *= Math.PI/180;
+  var a = this.equatorialRadius;
+  var b = this.polarRadius;
+  var e = Math.sqrt(2*this.flattening - Math.pow(this.flattening,2));
+  var N = a/Math.sqrt(1-Math.pow(e,2) * Math.pow(Math.sin(lat),2));
+  var x = (N + alt) * Math.cos(lat) * Math.cos(lon);
+  var y = (N + alt) * Math.cos(lat) * Math.sin(lon);
+  var z = (Math.pow(b,2)/Math.pow(a,2)*N+alt)*Math.sin(lat);
+  tweet.position = {x: x, y: y, z: z};
+  return tweet;
+};
+
 Globe.prototype.EcefToGeo = function (x, y, z) {
   var p = Math.sqrt(x * x + y * y);
   var e = Math.sqrt(2 * this.flattening - Math.pow(this.flattening, 2));
@@ -79,7 +94,7 @@ Globe.prototype.flash = function(params){
   var size = params['size'] || 1;
   var duration = params['duration'] || 1;
   var geo = this.geoToEcef(lat, lon, alt);
-  var arrow = new THREE.ArrowHelper(geo.clone().normalize(), geo.clone(), 0, new THREE.Color('orange'), undefined, 0)
+  var arrow = new THREE.ArrowHelper(geo.clone().normalize(), geo.clone(), 0, new THREE.Color(params.color), undefined, 0)
   this.add(arrow);
   var that = this;
   var updateArrow = function(){
@@ -99,3 +114,50 @@ Globe.prototype.flash = function(params){
       .chain(outTween)
       .start();
 };
+
+Globe.prototype.drawEdge = function(source, target, color, fade, width) {
+  fade = fade || false;
+  //var distance = latlonDistance(source.position, target.position);
+  var multiplier = 1.5;
+
+  //make a 3js line object
+  var material = new THREE.LineBasicMaterial( { color: 0xCCCCCC, opacity: 0.5, linewidth: width } );
+
+  //cache the coordinates of the source and target nodes
+  var sourceXy = source.position;
+  var targetXy = target.position;
+
+  //get averages (mid-point) between coordinates of source and target
+  var AvgX = (sourceXy.x + targetXy.x)/2;
+  var AvgY = (sourceXy.y + targetXy.y)/2;
+  var AvgZ = (sourceXy.z + targetXy.z)/2;
+  //get difference between source and target
+  var diffX = Math.abs(sourceXy.x - targetXy.x);
+  var diffY = Math.abs(sourceXy.y - targetXy.y);
+  //set middle point to average(x/y) and average(z + sum of difference(x/y))
+  var middle = [ AvgX * multiplier, AvgY * multiplier, AvgZ * multiplier ];
+
+  //make quadratic bezier out of the three points
+  var curve = new THREE.QuadraticBezierCurve3(new THREE.Vector3(sourceXy.x, sourceXy.y, sourceXy.z), new THREE.Vector3(middle[0], middle[1], middle[2]), new THREE.Vector3(targetXy.x, targetXy.y, targetXy.z));
+
+  //make a curve path and add the bezier curve to it
+  var path = new THREE.CurvePath();
+  path.add(curve);
+
+  //create material for our line
+  var curveMaterial = new THREE.LineBasicMaterial({
+    color: color, linewidth: 2, transparent: true
+  });
+
+  //create curved line and add to scene
+  var curvedLine = new THREE.Line(path.createPointsGeometry(100), curveMaterial);
+  curvedLine.lookAt(scene.position);
+  var onComplete = function(curvedLine){
+    scene.remove(curvedLine);
+  };
+  // if(fade){
+  //   curvedLine.material.transparent = true;
+  //   createjs.Tween.get(curvedLine.material).wait(5000).to({opacity: 0}, 5000).call(onComplete, [curvedLine]);
+  // }
+  scene.add(curvedLine);
+}
