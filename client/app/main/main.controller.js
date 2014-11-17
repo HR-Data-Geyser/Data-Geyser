3c5d8a09d88c763bd94a8b59e6c0c40d93e765c8
Brian Iversen <biversen21@gmail.com>
2014-11-17 11:43:35 -0800

    removes ajax call and handles gettopic in main.controller

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,9 +6,21 @@ angular.module('dataGeyserApp')
     $scope.awesomeTweets = [];
     $scope.tweetParser = [];
     $scope.topic = "ebola";
+    window.onkeydown = checkKeyPressed;
     
     var tweetTempStorage = {};
 
+    function checkKeyPressed(e){
+
+      if (e.keyCode === 32) {
+        e.preventDefault();
+        vrControls.zeroSensor();
+      }
+      if (e.keyCode === 13) {
+        e.preventDefault();
+        $scope.getTopic($scope.topic);
+      }
+    }
     
     $http.get('/api/tweets').success(function(awesomeTweets) {
       $scope.awesomeTweets = awesomeTweets;
@@ -62,7 +74,7 @@ angular.module('dataGeyserApp')
 
         console.log('get success');
         Interceptor.end();
-        // callback(data);
+        renderTweets(data);
       });
     }
     
@@ -73,3 +85,67 @@ angular.module('dataGeyserApp')
     }
     
   });
+  
+//////// simple function to calculate random value between -20 and 20 for fountain offset ////////
+
+var nodeTargetRandom = function(){
+  return Math.random() * (20 - (-20)) + (-20);
+}
+
+////////////// renders tweets in order gathered from DB ////////////////
+
+var renderTweets = function(tweets){
+  
+  var i = 1;
+  var renderLoop = function(){
+
+    var nodeSource = globe.getEcef(tweets[i].latitude, -tweets[i].longitude, 0);
+
+    if (tweets[i].followers_count > 10000 && addEdge) {
+      var multi = Math.ceil(tweets[i].followers_count / 10000);
+      var color = 'blue';
+      for (var j = 0; j < multi; j++) {              
+        var nodeTarget = globe.getEcef(tweets[i].latitude + nodeTargetRandom(), -tweets[i].longitude + nodeTargetRandom(), 0);
+        globe.drawEdge(nodeSource, nodeTarget, color, true, 5);
+      }
+      var url = tweets[i].photo;
+      // displayPhoto(url, nodeTarget);  // Uncomment to enable displayPhoto IF security disabled
+    } else {
+      var color = 'orange';
+    }
+    globe.spark({lat: tweets[i].latitude, lon: -tweets[i].longitude, size: 50, color: color, duration: 2 });
+    setTimeout(function(){
+      if (i < tweets.length) {
+        renderLoop(tweets[i++]);
+      }
+    }, 150);  // sets 150ms interval between tweets
+  }
+  renderLoop();
+}
+
+////////////////// displays photos flying into space //////////////////
+
+var displayPhoto = function(url, node){
+  
+  var onComplete = function(object){
+    scene.remove(object);
+  };
+  var pos = camera.position;
+  var rnd = Math.random;
+  var texture = new Image();
+  texture.crossOrigin = "";
+  texture.onload = function(){
+    var material = new THREE.MeshBasicMaterial( { map: new THREE.Texture(texture), side:THREE.DoubleSide, transparent: true } );
+    material.opacity = 0.7;
+    var imageGeometry = new THREE.PlaneBufferGeometry(texture.width / 10, texture.height / 10, 1, 1);
+    var image = new THREE.Mesh(imageGeometry, material);
+    image.position.set( node.position.x,node.position.y,node.position.z );
+    image.material.map.needsUpdate = true;
+    image.lookAt(camera.position);
+    scene.add(image);
+    createjs.Tween.get(image.position)
+    .to({x: pos.x*(0.9+(rnd()*0.4)), y: pos.y*(0.9+(rnd()*0.4)), z: pos.z*(0.9+(rnd()*0.4))}, 8000)
+    .call(onComplete, [image]); 
+  };
+  texture.src = url;
+};
