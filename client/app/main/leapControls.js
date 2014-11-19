764a3a401f56392c8896a0996a5cd9218915d1b2
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-11-19 10:41:42 -0800

    Verifying oculusIsOn, playing with screenPosition

--- client/app/main/leapControls.js
+++ client/app/main/leapControls.js
@@ -1,20 +1,15 @@
         /////////// Leap controls ////////////
 
-// var leapIsOn = false; 
 var leapController = new Leap.Controller({ enableGestures: true });
 
 var leapIsOn = true; 
-leapController.on('connect', function() {
-  console.log('InConnect');
-  // leapIsOn = true; 
-});
-leapController.on('ready', function () { dispatch.trigger('LeapControl:reconnect'); console.log('ready');  });
-console.log(leapIsOn); 
-
+var oculusIsOn = false; 
+console.log('1) OC ON? ', oculusIsOn); 
 if (leapIsOn) {
 
   console.log('LEEAAAPP'); 
   leapController.connect();
+  // leapController.use('screenPosition');   // Attempt to display handPos on screen 
   
   var dx = 0.001;
   var dy = 0.001;
@@ -34,21 +29,26 @@ if (leapIsOn) {
       var zoom = hand.palmPosition[1];
       var vel = hand.palmVelocity;
       var v = Math.sqrt(vel[0] * vel[0] + vel[1] * vel[1] + vel[2] * vel[2]);
+      var scaleZoom = zoom - 200;
       var scrollSpeed;
 
       var leapBoxLimits = {}; 
+
+
       if ( oculusIsOn ) {
+      console.log('2) OC ON? ', oculusIsOn); 
         leapBoxLimits.lr = 40; 
         leapBoxLimits.ud = 40;
         leapBoxLimits.zoomIn = 20;
-        leapBoxLimits.zoomOut = 50;
+        leapBoxLimits.zoomOut = 40;
       } else {
+      console.log('3) OC ON? ', oculusIsOn); 
+
         leapBoxLimits.lr = 80; 
         leapBoxLimits.ud = 70;
         leapBoxLimits.zoomIn = 20;
         leapBoxLimits.zoomOut = 70;
       }
-      var scaleZoom = zoom - 200;
 
       var leapSpeed = {}; 
       if ( oculusIsOn ) {
@@ -72,23 +72,23 @@ if (leapIsOn) {
         if(hand.grabStrength < 0.4){ //hand open
           if( Math.abs(lr) > leapBoxLimits.lr ){
             var direction = lr > 0 ? -1 : 1 ;
-            scrollSpeed = ( Math.abs(lr) - 80 ) / leapSpeed.lr;
+            scrollSpeed = ( Math.abs(lr) - leapBoxLimits.lr ) / leapSpeed.lr;
             orbitControls.rotateLeft(direction * scrollSpeed);
           }
           if( Math.abs(ud) > leapBoxLimits.ud ){
             var direction = ud > 0 ? -1 : 1 ;
-            scrollSpeed = ( Math.abs(ud) - 80 ) / leapSpeed.ud;
+            scrollSpeed = ( Math.abs(ud) - leapBoxLimits.ud ) / leapSpeed.ud;
             orbitControls.rotateUp(direction * scrollSpeed);
           }
           if( scaleZoom < leapBoxLimits.zoomIn || scaleZoom > leapBoxLimits.zoomOut ) {
             var offset = scaleZoom;
-            if(offset < 20) {
-              scrollSpeed = Math.abs(offset - 20) / leapSpeed.zoom ;
+            if(offset < leapBoxLimits.zoomIn) {
+              scrollSpeed = Math.abs(offset - leapBoxLimits.zoomIn) / leapSpeed.zoom ;
               orbitControls.dollyIn(1 + scrollSpeed);
             }
-            if (offset > 80){
-              scrollSpeed = Math.abs(offset - 80) / leapSpeed.zoom ;
-              orbitControls.dollyOut(1.0 + scrollSpeed);
+            if (offset > leapBoxLimits.zoomOut){
+              scrollSpeed = Math.abs(offset - leapBoxLimits.zoomOut) / leapSpeed.zoom ;
+              orbitControls.dollyOut(1 + scrollSpeed);
             }
           }
           orbitControls.update()
@@ -96,7 +96,7 @@ if (leapIsOn) {
         if(hand.grabStrength > 0.8){
 
           if(nextFuncReady){
-            getTweets();
+            // getTweets();
             nextFuncReady = false;
             setTimeout(function(){nextFuncReady = true;}, 10000);
           }
@@ -108,7 +108,6 @@ if (leapIsOn) {
         var gesture = frame.gestures[g]; 
 
         var type = gesture.type;
-        console.log('frame gestures: ', frame.gestures); 
 
         if (type === 'swipe') {
           onSwipe(gesture); 
