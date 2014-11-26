4c7e3e545379b32e3fb41d445192d6bb1bedcf75
Brian Iversen <biversen21@gmail.com>
2014-11-26 14:33:00 -0800

    adds additional mocha tests and reinstates karma

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -4,26 +4,26 @@ describe('Unit: MainCtrl', function () {
   // load the controller's module
   
   beforeEach(module('dataGeyserApp'));
-  // beforeEach(module('socketMock'));
+  beforeEach(module('socketMock'));
 
-  // var MainCtrl,
-  //     scope,
-  //     $httpBackend;
-  //
-  // // Initialize the controller and a mock scope
-  // beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-  //   $httpBackend = _$httpBackend_;
-  //   $httpBackend.expectGET('/api/tweets')
-  //     .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
-  //
-  //   scope = $rootScope.$new();
-  //   MainCtrl = $controller('MainCtrl', {
-  //     $scope: scope
-  //   });
-  // }));
-  //
-  // it('should attach a list of tweets to the scope', function () {
-  //   $httpBackend.flush();
-  //   expect(scope.awesomeTweets.length).toBe(4);
-  // });
+  var MainCtrl,
+      scope,
+      $httpBackend;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/api/tweets')
+      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+
+    scope = $rootScope.$new();
+    MainCtrl = $controller('MainCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of tweets to the scope', function () {
+    $httpBackend.flush();
+    expect(scope.awesomeTweets.length).toBe(4);
+  });
 });
